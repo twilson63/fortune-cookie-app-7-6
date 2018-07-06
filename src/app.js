56405ec6f09527137676ba33f14c9d39ca84ec3f
Tom Wilson <tom@jackhq.com>
2018-07-06 14:15:35 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import fortuneCookie from 'fortune-cookie'
+import style from './app.css.js'
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+const App = props => {
+  const state = props.state
+  const setState = props.setState
+
+  const getFortune = () => {
+    const number = getRandomInt(fortuneCookie.length - 1)
+    return {
+      fortune: fortuneCookie[number]
+    }
+  }
+
+  return (
+    <div style={style.root}>
+      <h1>Fortune Cookie App</h1>
+      <h3 className="mh2 fw1 ba pa4 br2 bg-light-blue">{state.fortune}</h3>
+      <button
+        style={style.button}
+        onClick={() => {
+          setState(getFortune)
+        }}
+      >
+        Get Fortune
+      </button>
+      <p className="instructions">Click button to get fortune</p>
+    </div>
+  )
+}
+
+export default App
